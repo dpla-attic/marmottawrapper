4093a38d7415d64c8a1e9dc517a1d231bdecfdd7
Mark A. Matienzo <mark@dp.la>
2014-10-10 09:44:46 -0500

    Rename gem; clean up Rake tasks and old Maven project files

--- lib/marmottawrapper.rb
+++ lib/marmottawrapper.rb
@@ -1,5 +1,4 @@
 require 'fileutils'
-require 'childprocess'
 
 class Marmottawrapper
   class << self
@@ -42,93 +41,28 @@ class Marmottawrapper
     end
 
     def install
+      fetch unless File.exists?(File.join(tmp_dir, 'tomcat-marmotta.zip'))
       system "unzip #{tmp_dir}/tomcat-marmotta.zip"
       File.delete(File.join(tmp_dir, 'tomcat-marmotta.zip'))
     end 
 
     ##
-    # Start the Tomcat server with Marmotta on port 8080
+    # Start the Tomcat server with Marmotta 
     def start
-      # if pid
-      #   if is_running?
-      #     raise("Server is already running with PID #{pid}")
-      #   else
-      #     puts "Removing stale PID file at #{pid_path}"
-      #     File.delete(pid_path)
-      #   end
-      # end
-      
-      # Dir.chdir(marmotta_dir) {
-      #   process.start
-      # }
-
-      # FileUtils.makedirs(pid_dir) unless File.directory?(pid_dir)
-      # File::open(pid_path, 'w') do |f|
-      #   f.puts "#{process.pid}"
-      # end
-
-      # process.pid
       system "#{tomcat_dir}/bin/startup.sh"
     end
     
     ##
     # Stop Tomcat/Marmotta
     def stop
-      # raise "No pid found in #{pid_path}" unless pid or @process
-      # if @process
-      #   @process.stop
-      # else
-      #   Process.kill("KILL", pid) rescue nil
-      # end
-      
-      # begin
-      #   File.delete(pid_path)
-      # rescue
-      # end
       system "#{tomcat_dir}/bin/shutdown.sh"
     end
 
     def restart
       stop
+      sleep 2
       start
     end
 
-    def process
-      @process ||= begin
-                     process = ChildProcess.build('mvn', 'clean', 'tomcat7:run')
-                     # TODO: When use this instead of the above
-                     # java -jar #{build_dir}/marmotta-webapp-3.2.1-0.0.1-war-exec.jar
-                     process.detach = true
-                     process
-                   end
-    end
-
-    def is_running?  
-      begin
-        return Process.getpgid(pid) != -1
-      rescue Errno::ESRCH
-        return false
-      end
-    end
-
-    def pid
-      File.open( pid_path ) { |f| return f.gets.to_i } if File.exist?(pid_path)
-    end
-
-    def pid_path
-      @path = File.join(pid_dir, pid_file)
-    end
-
-    def pid_dir
-      File.expand_path(File.join(app_root, 'tmp'))
-    end
-
-    def pid_file
-      'tomcat.pid'
-    end
-
-    def pid_file?
-      File.exist?(pid_path)
-    end
   end
 end
